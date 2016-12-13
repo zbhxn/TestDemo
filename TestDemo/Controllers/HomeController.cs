3a35aa803271b618d8dc883cfad9d975d2061174
zhangbing <278895148@qq.com>
2016-12-13 10:16:07 +0800

    Add

---
+++ TestDemo/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["DataMsg"]= "TestDemoViewData";
+            ViewBag.BagMsg = "TestDemoViewBag";
+            return View();
+        }
+    }
+}
