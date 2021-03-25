d40486be1e4a3867322fb35d00d7b5a56c699e9c
kevin-pki <79350043+kevin-pki@users.noreply.github.com>
2021-03-25 12:32:03 +0800

    TestDemo项目重命名成ConnectionAzureSQL
    TestDemo项目重命名成ConnectionAzureSQL

--- ConnectionAzureSQL/Properties/AssemblyInfo.cs
+++ ConnectionAzureSQL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("TestDemo")]
+[assembly: AssemblyTitle("ConnectionAzureSQL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestDemo")]
+[assembly: AssemblyProduct("ConnectionAzureSQL")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
