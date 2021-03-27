6580f0859d27bc01ceb12220e5d21576ce14c1ad
kevin-pki <79350043+kevin-pki@users.noreply.github.com>
2021-03-28 00:52:00 +0800

    创建SignalRChat项目
    参照https://docs.microsoft.com/zh-cn/aspnet/signalr/overview/getting-started/tutorial-getting-started-with-signalr

---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
