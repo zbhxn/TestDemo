1ef5775ac99afc97dead4ce85a2c4ac3342e3f00
kevin-pki <278895148@qq.com>
2021-04-08 23:21:52 +0800

    创建MoveShapeDemo

---
+++ MoveShapeDemo/MoveShapeHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoveShapeDemo
+{
+    public class MoveShapeHub : Hub
+    {
+        public void UpdateModel(ShapeModel clientModel)
+        {
+            clientModel.LastUpdatedBy = Context.ConnectionId;
+            // Update the shape model within our broadcaster
+            Clients.AllExcept(clientModel.LastUpdatedBy).updateShape(clientModel);
+        }
+    }
+
+    public class ShapeModel
+    {
+        //我们使用JsonProperty将Left和Top声明为小写，以同步客户端和服务器模型
+        [JsonProperty("left")]
+        public double Left { get; set; }
+        [JsonProperty("top")]
+        public double Top { get; set; }
+        //我们不希望客户端获取“LastUpdatedBy”属性
+        [JsonIgnore]
+        public string LastUpdatedBy { get; set; }
+    }
+}
---
+++ MoveShapeDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MoveShapeDemo.Startup))]
+
+namespace MoveShapeDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
