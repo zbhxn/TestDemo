fefe9529181cda3b71f77601f5e59b945e2cd862
zbhxn <278895148@qq.com>
2021-04-19 22:33:16 +0800

    Expression和ConfigurationManager Demo

---
+++ ConfigurationManagerDemo/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConfigurationManagerDemo
+{
+    class Program
+    {
+        /*
+          重新生成解决方案：清理+生成
+         */
+        static void Main(string[] args)
+        {
+            ReadAllSettings();
+            ReadSetting("Setting1");
+            ReadSetting("NotValid");
+            AddUpdateAppSettings("NewSetting", "May 7, 2014");
+            AddUpdateAppSettings("Setting1", "May 8, 2014");
+            ReadAllSettings();
+
+            ReadAllConnectionStrings();
+            Console.ReadKey();
+        }
+
+        static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                Console.WriteLine(result);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        static void ReadAllConnectionStrings()
+        {
+            try
+            {
+                ConnectionStringSettingsCollection connectionStrings = ConfigurationManager.ConnectionStrings;
+
+                if (connectionStrings.Count == 0)
+                {
+                    Console.WriteLine("ConnectionStrings is empty.");
+                }
+                else
+                {
+                    foreach (ConnectionStringSettings item in connectionStrings)
+                    {
+                        Console.WriteLine(item.GetType());
+                        Console.WriteLine("Key: {0} Value: {1}", item.Name, item.ConnectionString);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+    }
+}
