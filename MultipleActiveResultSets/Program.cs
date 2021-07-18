ed76d0f9e404884a7c2bdac9832f37d37143a02e
zbhxn <278895148@qq.com>
2021-07-19 00:26:55 +0800

    MultipleActiveResultSets

---
+++ MultipleActiveResultSets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleActiveResultSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder
+            {
+                DataSource = ".",
+                InitialCatalog = "Test",
+                IntegratedSecurity = true,
+                MultipleActiveResultSets = true
+            };
+
+            using (SqlConnection con = new SqlConnection(sb.ConnectionString))
+            {
+                con.Open();
+
+                string sql = "select count(0) from [user];select * from [user];";
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        using (DataSet dataSet = new DataSet())
+                        {
+                            adapter.Fill(dataSet, "Anonymous");
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
