3496b008a46b535090433b975ed5310929a46f48
zbhxn <278895148@qq.com>
2021-09-06 23:41:00 +0800

    LINQ之SelectMany用法

---
+++ Core-SelectMany/Person.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Core_SelectMany
+{
+    public class Person
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public string Gender { set; get; }
+        public Dog[] Dogs { set; get; }
+
+        public static List<Person> Persons
+        {
+            get
+            {
+                return new List<Person>
+                {
+                    new Person
+                    {
+                        Name = "P1", Age = 18, Gender = "Male",
+                        Dogs = new Dog[]
+                        {
+                            new Dog { Name = "D1" },
+                            new Dog { Name = "D2" }
+                        }
+                    },
+                    new Person
+                    {
+                        Name = "P2", Age = 19, Gender = "Male",
+                        Dogs = new Dog[]
+                        {
+                            new Dog { Name = "D3" }
+                        }
+                    },
+                    new Person
+                    {
+                        Name = "P3", Age = 17,Gender = "Female",
+                        Dogs = new Dog[]
+                        {
+                            new Dog { Name = "D4" },
+                            new Dog { Name = "D5" },
+                            new Dog { Name = "D6" }
+                        }
+                    }
+                };
+            }
+        }
+    }
+
+    public class Dog
+    {
+        public string Name { set; get; }
+    }
+}
---
+++ Core-SelectMany/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core_SelectMany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Select和SelectMany区别
+            string[] text = { "Today is 2018-06-06", "weather is sunny", "I am happy" };
+            var tokens = text.Select(s => s.Split(' '));
+            var tokens2 = text.SelectMany(s => s.Split(' '));
+
+            //LINQ之SelectMany用法
+            //https://www.cnblogs.com/cncc/p/9840463.html
+
+            //一、第一种用法：
+            //public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector);
+            var dogs = Person.Persons.SelectMany(p => p.Dogs);
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog.Name);
+            }
+
+            dogs = from p in Person.Persons
+                   from d in p.Dogs
+                   select d;
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog.Name);
+            }
+
+            //2、第二种用法：
+            //public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TResult>> selector);
+            dogs = Person.Persons.SelectMany((p, i) => p.Dogs.Select(d =>
+                                             {
+                                                 d.Name = $"{i},{d.Name}";
+                                                 return d;
+                                             }));
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog.Name);
+            }
+
+            //三、第三种用法：
+            //public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector);
+            var results = Person.Persons.SelectMany(p => p.Dogs, (p, d) => new { PersonName = p.Name, DogName = d.Name });
+            foreach (var result in results)
+            {
+                Console.WriteLine($"{result.PersonName},{result.DogName}");
+            }
+
+            results = from p in Person.Persons
+                      from d in p.Dogs
+                      select new { PersonName = p.Name, DogName = d.Name };
+            foreach (var result in results)
+            {
+                Console.WriteLine($"{result.PersonName},{result.DogName}");
+            }
+
+            //四、第四种用法：
+            //public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(this IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector);
+            results = Person.Persons.SelectMany((p, i) =>
+            {
+                for (int j = 0; j < p.Dogs.Length; j++)
+                {
+                    p.Dogs[j].Name = $"{i}-{p.Dogs[j].Name}";
+                }
+                return p.Dogs;
+            }, (p, d) => new { PersonName = p.Name, DogName = d.Name });
+            foreach (var result in results)
+            {
+                Console.WriteLine($"{result.PersonName},{result.DogName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
